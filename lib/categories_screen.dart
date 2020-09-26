42e15d2ff1eb5978d3a5c8e57550d78a3274761d
Sravan <sravanpabolu@gmail.com>
2020-09-26 18:33:09 +0530

    Category screen, Category Item, Dummy data

---
+++ lib/categories_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import './category_item.dart';
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DeliMeal'),
+      ),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2, //height and width
+          crossAxisSpacing: 20, //width
+          mainAxisSpacing: 20, //height
+        ),
+        children: DUMMY_CATEGORIES
+            .map(
+              (catData) => CategoryItem(
+                catData.title,
+                catData.color,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
