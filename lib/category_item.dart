801e5fe81d8ae9bddb5431f3280e9d1a929561ff
Sravan <sravanpabolu@gmail.com>
2020-09-26 22:58:12 +0530

    Using Named arguments to push

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -10,16 +10,12 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return CategoryMealsScreen(
-            id,
-            title,
-            // color,
-          );
-        },
-      ),
+    Navigator.of(ctx).pushNamed(
+      './category-meals',
+      arguments: {
+        'id': id,
+        'title': title,
+      },
     );
   }
 
--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  final String categoryID;
-  final String categoryTitle;
-  // final String categoryColor;
+  // final String categoryID;
+  // final String categoryTitle;
+  // // final String categoryColor;
 
-  CategoryMealsScreen(
-    this.categoryID,
-    this.categoryTitle,
-    // this.categoryColor,
-  );
+  // CategoryMealsScreen(
+  //   this.categoryID,
+  //   this.categoryTitle,
+  //   // this.categoryColor,
+  // );
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routeArgs['title'];
+    final categoryId = routeArgs['id'];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
