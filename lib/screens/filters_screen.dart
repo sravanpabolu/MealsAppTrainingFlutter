6511e1c74e6b971779bb89af5ee29ae58458d775
Sravan <sravanpabolu@gmail.com>
2020-10-09 01:48:11 +0530

    175. Replace pages, instead of pushing

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,3 +1,4 @@
+import '../widgets/main_drawer.dart';
 import 'package:flutter/material.dart';
 
 class FiltersScreen extends StatelessWidget {
@@ -5,8 +6,14 @@ class FiltersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Filters'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Filters'),
+      ),
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('Filters'),
+      ),
     );
   }
 }
